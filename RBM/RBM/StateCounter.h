c3e5ef37912cb66b9b63884b4edbc71a0aa0a9d8
user <user@localhost>
2017-08-09 03:28:00 +0900

    new state counter

--- RBM/RBM/StateCounter.h
+++ RBM/RBM/StateCounter.h
@@ -9,7 +9,7 @@ class StateCounter
 protected:
 	size_t _innerCounter = 0;  // 内部状態数カウンター
 	size_t _maxCount = 0;  // 状態数カウンターの最大値
-	T _steteSet;  // 各状態数
+	T _stateSet;  // 各状態数
 	T _stateCounterSet;
 
 	//
@@ -17,21 +17,23 @@ protected:
 	//
 	void _calcState() {
 		for (int n = 0; n < _stateSet.size(); n++) {
-			auto & s = _stateCounterSet;
-			s[n] = _innerCounter % std::accumulate(s.begin(), s.begin() + 1 + n, 1, [](int init, int i) {
-				return init * i; 
+			auto & s = _stateSet;
+			auto & sc = _stateCounterSet;
+
+			sc[n] = _innerCounter % std::accumulate(s.begin(), s.begin() + 1 + n, 1, [](int init, int i) {
+				return init * i;
 			}) / std::accumulate(s.begin(), s.begin() + n, 1, [](int init, int i) {
-				return init * i; 
+				return init * i;
 			});
 		}
 	}
 
-public: 
+public:
 	StateCounter(T state_set = {}) {
-		_stateSet = stateSet;
-		_stateCounterSet(stateSet);
+		_stateSet = state_set;
+		_stateCounterSet = state_set;
 		std::fill(_stateCounterSet.begin(), _stateCounterSet.end(), 0);
-		_maxCount = std::accumulate(stateSet.begin(), stateSet.end(), 1, [](int init, int i) { return init * i; });
+		_maxCount = std::accumulate(state_set.begin(), state_set.end(), 1, [](int init, int i) { return init * i; });
 	}
 
 	~StateCounter() = default;
@@ -45,6 +47,7 @@ public:
 	}
 
 	T getState() {
+		_calcState();
 		return _stateCounterSet;
 	}
 
