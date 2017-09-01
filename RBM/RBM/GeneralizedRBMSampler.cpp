d7e6e4651680e439bba6f9f2bdc8d0cbf11c7069
user <user@localhost>
2017-09-02 02:07:29 +0900

    bug fix issue #5

--- RBM/RBM/GeneralizedRBMSampler.cpp
+++ RBM/RBM/GeneralizedRBMSampler.cpp
@@ -1,4 +1,4 @@
-#include "GeneralizedRBMSampler.h"
+﻿#include "GeneralizedRBMSampler.h"
 #include "GeneralizedRBM.h"
 #include <random>
 
@@ -24,17 +24,44 @@ double GeneralizedRBMSampler::gibbsSamplingVisible(GeneralizedRBM & rbm, int vin
 }
 
 double GeneralizedRBMSampler::gibbsSamplingHidden(GeneralizedRBM & rbm, int hindex) {
-    std::vector<double> probs(rbm.getHiddenValueSetSize());
-    auto hidset = rbm.splitHiddenSet();
-    for (int i = 0; i < hidset.size(); i++) {
-        probs[i] = rbm.condProbHid(hindex, hidset[i]);
-    }
+    // 離散型
+	auto sample_discrete = [&] {
+		std::vector<double> probs(rbm.getHiddenValueSetSize());
+		auto hidset = rbm.splitHiddenSet();
+		for (int i = 0; i < hidset.size(); i++) {
+			// FIXME: 分配関数の計算を使いまわせば高速化可能
+			probs[i] = rbm.condProbHid(hindex, hidset[i]);
+		}
 
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::discrete_distribution<> dist(probs.begin(), probs.end());
+		std::random_device rd;
+		std::mt19937 mt(rd());
+		std::discrete_distribution<> dist(probs.begin(), probs.end());
 
-    double value = hidset[dist(mt)];
+		double value = hidset[dist(mt)];
+
+		return value;
+	};
+
+	// 連続型
+	auto sample_real = [&] {
+		// 連続値は逆関数法で
+		std::random_device rd;
+		std::mt19937 mt(rd());
+		std::uniform_real_distribution<double> dist(0.0, 1.0);
+		auto u = dist(mt);
+		auto h_max = rbm.getHiddenMax();
+		auto h_min = rbm.getHiddenMin();
+
+		auto mu_j = rbm.mu(hindex);
+		auto z_j = (exp(h_max * mu_j) - exp(h_min * mu_j)) / mu_j;
+
+		double value = log(z_j * u * mu_j + exp(h_min * mu_j)) / mu_j;
+
+		return value;
+	};
+
+	auto value = rbm.isRealHiddenValue() ? sample_real() : sample_discrete();
+	
     return value;
 }
 
@@ -59,28 +86,13 @@ Eigen::VectorXd & GeneralizedRBMSampler::blockedGibbsSamplingHidden(GeneralizedR
 }
 
 double GeneralizedRBMSampler::updateByGibbsSamplingVisible(GeneralizedRBM & rbm, int vindex) {
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::uniform_real_distribution<double> dist(0.0, 1.0);
-
-    double value = dist(mt) < rbm.condProbVis(vindex, 0.0) ? 0.0 : 1.0;
+	auto value = gibbsSamplingVisible(rbm, vindex);
     rbm.nodes.v(vindex) = value;
     return value;
 }
 
 double GeneralizedRBMSampler::updateByGibbsSamplingHidden(GeneralizedRBM & rbm, int hindex) {
-    std::vector<double> probs(rbm.getHiddenValueSetSize());
-    auto hidset = rbm.splitHiddenSet();
-    for (int i = 0; i < hidset.size(); i++) {
-        probs[i] = rbm.condProbHid(hindex, hidset[i]);
-    }
-
-    std::random_device rd;
-    std::mt19937 mt(rd());
-    std::discrete_distribution<> dist(probs.begin(), probs.end());
-
-    int index = dist(mt);
-    double value = hidset[index];
+	auto value = gibbsSamplingHidden(rbm, hindex);
     rbm.nodes.h(hindex) = value;
     return value;
 }
