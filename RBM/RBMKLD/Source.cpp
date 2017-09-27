1106c7ac3a23a14686fa294468171d5644cbaa4b
user <user@localhost>
2017-09-27 20:24:31 +0900

    refactor

--- RBM/RBMKLD/Source.cpp
+++ RBM/RBMKLD/Source.cpp
@@ -9,14 +9,26 @@
 int main(void) {
 	int vsize = 6;
 	int hsize = 10;
-	auto rbm_gen = GeneralizedRBM(vsize, hsize);
-	auto rbm_t = GeneralizedRBM(vsize, hsize);
+	int datasize = 1000;
+	int epoch = 100;
+	int batchsize = datasize/10;
+	double learning_rate = 0.01;
 
-	auto rbm_train = GeneralizedRBMTrainer(rbm_t);
-	rbm_train.epoch = 1;
-	rbm_train.cdk = 1;
-	rbm_train.batchSize = 10;
-	rbm_train.learningRate = 0.01;
+	auto rbm_gen = GeneralizedRBM(vsize, hsize);
+	auto rbm_exact = GeneralizedRBM(vsize, hsize);
+	auto rbm_cd = GeneralizedRBM(vsize, hsize);
+
+	auto rbm_trainer_exact = GeneralizedRBMTrainer(rbm_exact);
+	rbm_trainer_exact.epoch = epoch;
+	rbm_trainer_exact.cdk = 1;
+	rbm_trainer_exact.batchSize = batchsize;
+	rbm_trainer_exact.learningRate = learning_rate;
+
+	auto rbm_trainer_cd = GeneralizedRBMTrainer(rbm_cd);
+	rbm_trainer_cd.epoch = epoch;
+	rbm_trainer_cd.cdk = 1;
+	rbm_trainer_cd.batchSize = batchsize;
+	rbm_trainer_cd.learningRate = learning_rate;
 
 	std::vector<std::vector<double>> dataset;
 	for (int i = 0; i < 1000; i++) {
@@ -24,10 +36,17 @@ int main(void) {
 	}
 
 
+	std::cout << "[Exact]" << std::endl;
+	std::cout << rbmutil::kld(rbm_gen, rbm_exact, std::vector<int>{0, 1}) << std::endl;
+	rbm_trainer_exact.trainExact(rbm_exact, dataset);
+	std::cout << rbmutil::kld(rbm_gen, rbm_exact, std::vector<int>{0, 1}) << std::endl;
 
-	std::cout << rbmutil::kld(rbm_gen, rbm_t, std::vector<int>{0, 1}) << std::endl;
-	rbm_train.trainExact(rbm_t, dataset);
-	std::cout << rbmutil::kld(rbm_gen, rbm_t, std::vector<int>{0, 1}) << std::endl;
+	std::cout << std::endl;
+
+	std::cout << "[Contrastive Divergence]" << std::endl;
+	std::cout << rbmutil::kld(rbm_gen, rbm_cd, std::vector<int>{0, 1}) << std::endl;
+	rbm_trainer_cd.trainCD(rbm_cd, dataset);
+	std::cout << rbmutil::kld(rbm_gen, rbm_cd, std::vector<int>{0, 1}) << std::endl;
 
 	return 0;
 }
