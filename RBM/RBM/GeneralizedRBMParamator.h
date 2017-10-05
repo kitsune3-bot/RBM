5183f39355d1ebb9bc00e3811b5e8601017e8a83
user <user@localhost>
2017-10-06 01:45:33 +0900

    to inline

--- RBM/RBM/GeneralizedRBMParamator.h
+++ RBM/RBM/GeneralizedRBMParamator.h
@@ -55,6 +55,45 @@ public:
 	void deserialize(std::string js);
 };
 
+inline GeneralizedRBMParamator::GeneralizedRBMParamator(size_t v_size, size_t h_size) {
+	vSize = v_size;
+	hSize = h_size;
+
+	initParams();
+}
+
+inline void GeneralizedRBMParamator::initParams() {
+	b.resize(vSize);
+	b.setConstant(0.0);
+	c.resize(hSize);
+	c.setConstant(0.0);
+	w.resize(vSize, hSize);
+	w.setConstant(0.0);
+}
+
+inline void GeneralizedRBMParamator::initParamsRandom(double range_min, double range_max) {
+	b.resize(vSize);
+	c.resize(hSize);
+	w.resize(vSize, hSize);
+
+	std::random_device rd;
+	std::mt19937 mt(rd());
+	std::uniform_real_distribution<double> dist(range_min, range_max);
+
+	for (int i = 0; i < vSize; i++) {
+		b(i) = dist(mt);
+
+		for (int j = 0; j < hSize; j++) {
+			w(i, j) = dist(mt);
+		}
+	}
+
+	for (int j = 0; j < hSize; j++) {
+		c(j) = dist(mt);
+	}
+}
+
+
 // 可視変数の総数を返す
 inline size_t GeneralizedRBMParamator::getVisibleSize() {
     return vSize;
